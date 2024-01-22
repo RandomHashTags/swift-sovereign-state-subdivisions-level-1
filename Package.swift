dea486c4387135f289b679ce4908b4a82a9392ee
RandomHashTags <ImRandomHashTags@gmail.com>
2024-01-22 17:04:30 -0600

    moved to own repo

--- Package.swift
+++ Package.swift
@@ -1,23 +1,39 @@
-// swift-tools-version: 5.9
+// swift-tools-version:5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "swift-sovereign-state-subdivisions-level-1",
+    defaultLocalization: "en",
+    platforms: [
+        .macOS(.v13),
+        .iOS(.v16),
+        .tvOS(.v16),
+        .watchOS(.v9)
+    ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "swift-sovereign-state-subdivisions-level-1",
-            targets: ["swift-sovereign-state-subdivisions-level-1"]),
+            name: "SwiftSovereignStateSubdivisionsLevel1",
+            targets: ["SwiftSovereignStateSubdivisionsLevel1"]
+        ),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/RandomHashTags/swift-sovereign-states.git", branch: "main")
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "swift-sovereign-state-subdivisions-level-1"),
+            name: "SwiftSovereignStateSubdivisionsLevel1",
+            dependencies: [
+                .product(name: "SwiftSovereignStates", package: "swift-sovereign-states")
+            ],
+            resources: [
+                .process("Resources")
+            ]
+        ),
         .testTarget(
-            name: "swift-sovereign-state-subdivisions-level-1Tests",
-            dependencies: ["swift-sovereign-state-subdivisions-level-1"]),
+            name: "SwiftSovereignStateSubdivisionsLevel1Tests",
+            dependencies: ["SwiftSovereignStateSubdivisionsLevel1"]
+        ),
     ]
 )
---
+++ Tests/SwiftSovereignStateSubdivisionsLevel1Tests/SwiftSovereignStateSubdivisionsLevel1Tests.swift
@@ -0,0 +1,14 @@
+//
+//  SwiftSovereignStateSubdivisionsLevel1Tests.swift
+//
+//
+//  Created by Evan Anderson on 1/22/24.
+//
+
+import XCTest
+@testable import SwiftSovereignStateSubdivisionsLevel1
+
+final class SwiftSovereignStateSubdivisionsLevel1Tests : XCTestCase {
+    func testExample() throws {
+    }
+}
